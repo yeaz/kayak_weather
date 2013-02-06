7c4b5ae9b237172ed6e8758e185f011b2332e317
yeaz <yeaz@stanford.edu>
2013-02-07 08:29:48 +0900

    inputted api_key provided by Kayak

--- lib/weatherbug.rb
+++ lib/weatherbug.rb
@@ -6,18 +6,19 @@ class Weatherbug
   format :json
   base_uri 'http://i.wxbug.net'
 
-  def self.getForecast(la, lo, i)
-    api_key = ['hm9jtjrw5q3b6f95rj4kcpcz', 
-               's5ud8b4gw5ufatuktycbf4uz', 
-               '9xkqb2g2m6hphbusuvhu5qpg',
-               '2mjgqssbb3epdq2gh4r9c2km',
-               'j6m4tk9hs4f682egneujdu83',
-               'vtnmenqmvqryjuswwe7ptcdc',
-               'em5vjqu3xxf9p63b2zwyjc7n',
-               'dvdkjwc7rr5py8632efe39t5',
-               'hx95um8hc5z2fuxnpuhqf2e7',
-               'a5xpmr86e5apeb3qs46qbh5z']
-    get('/REST/Direct/GetForecast.ashx?la=' + la + '&lo=' + lo + '&nf=7&ht=t&l=en&c=US&api_key=' + api_key[i])
+  def self.getForecast(la, lo)
+    api_key = 'fbzpn6ambx9de5nusf49rv4d'
+    #api_key = ['hm9jtjrw5q3b6f95rj4kcpcz', 
+    #           's5ud8b4gw5ufatuktycbf4uz', 
+    #           '9xkqb2g2m6hphbusuvhu5qpg',
+    #           '2mjgqssbb3epdq2gh4r9c2km',
+    #           'j6m4tk9hs4f682egneujdu83',
+    #           'vtnmenqmvqryjuswwe7ptcdc',
+    #           'em5vjqu3xxf9p63b2zwyjc7n',
+    #           'dvdkjwc7rr5py8632efe39t5',
+    #           'hx95um8hc5z2fuxnpuhqf2e7',
+    #           'a5xpmr86e5apeb3qs46qbh5z']
+    get('/REST/Direct/GetForecast.ashx?la=' + la + '&lo=' + lo + '&nf=7&ht=t&l=en&c=US&api_key=' + api_key)
   end
 
 end
